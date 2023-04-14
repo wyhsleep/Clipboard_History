1dd483d43c583916d00e4d7797362b470aec5d4a
WangYiHui <wangyh265@gmail.com>
2023-04-14 21:26:27 +0800

    Initial Commit

---
+++ Clipboard_History/Clipboard_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_HistoryApp.swift
+//  Clipboard_History
+//
+//  Created by 汪奕晖 on 2023/4/14.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clipboard_History/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Clipboard_History
+//
+//  Created by 汪奕晖 on 2023/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
